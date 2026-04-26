25a26f116313e235fd73eedc90d462b5ac26ad55
Alexwcjung <127401494+Alexwcjung@users.noreply.github.com>
2026-04-26 17:44:21 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -1,11 +1,9 @@
 import streamlit as st
 
-st.set_page_config(layout="wide")
-
 st.markdown("#### Spring 2026")
 st.caption("This page is continuously updated.")
 
-IMAGE_URL = "https://raw.githubusercontent.com/Alexwcjung/Fun-English/dbc770e9321a0daa76131322ee9689ccb2cad370/a143182b-832c-4a27-87fb-74214eabb338.png"
+IMAGE_URL = "https://raw.githubusercontent.com/Alexwcjung/Fun-English/main/a143182b-832c-4a27-87fb-74214eabb338.png"
 
 st.image(
     IMAGE_URL,
